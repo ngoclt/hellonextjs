092884fb5697ff2f6187409d16759112a1433c41
Ngoc LE <ngoc.le@smarp.com>
2018-12-12 21:43:55 +0200

    Fetch data from tvmaze

--- pages/post.js
+++ pages/post.js
@@ -1,17 +1,22 @@
-import { withRouter } from "next/router";
 import MainLayout from "../components/MainLayout.js";
+import fetch from "isomorphic-unfetch";
 
-const Content = withRouter(props => (
-  <div>
-    <h1>{props.router.query.title}</h1>
-    <p>This is the blog post content.</p>
-  </div>
-));
-
-const Page = props => (
+const Post = props => (
   <MainLayout>
-    <Content />
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?p>/g, "")}</p>
+    <img src={props.show.image.medium} />
   </MainLayout>
 );
 
-export default Page;
+Post.getInitialProps = async function(context) {
+  const { id } = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+
+  console.log(`Fetched show: ${show.name}`);
+
+  return { show };
+};
+
+export default Post;
